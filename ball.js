6ae7c77570d6eaf0d4428180beae9b14783e7270
Moth Otter <mothotter@gmail.com>
2017-11-19 21:08:03 -0700

    starting code
    background basically

---
+++ ball.js
@@ -0,0 +1,6 @@
+function Ball() {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.xSpeed = 5;
+    this.ySpeed = 5;
+}
---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(800, 400);
+    background(0, 100, 0);
+};
+
+function draw() {
+    rect((width / 2) - 5, -1, 10, height + 1);
+};
